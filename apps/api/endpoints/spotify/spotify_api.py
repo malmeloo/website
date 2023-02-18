639655fdf2b73509b2b6e8bd2db90274c4614bcd
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2023-02-18 19:44:28 +0100

    Finalize Spotify login integration

---
+++ apps/api/endpoints/spotify/spotify_api.py
@@ -0,0 +1,119 @@
+import base64
+import json
+import sys
+from datetime import timedelta
+from urllib.parse import urlencode
+
+import httpx
+from django.conf import settings
+from django.utils import timezone
+
+from apps.api.models import OAuthToken
+
+CLIENT_ID = settings.CONFIG.get('spotify.clientId')
+CLIENT_SECRET = settings.CONFIG.get('spotify.clientSecret')
+
+API_ENDPOINT = 'https://api.spotify.com/v1'
+TOKEN_URL = 'https://accounts.spotify.com/api/token'
+AUTHORIZE_CALLBACK = 'https://accounts.spotify.com/authorize'
+SCOPES = 'user-read-email'
+
+HEADERS = {
+    'Authorization': 'Basic ' + base64.b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode()).decode()
+}
+
+client = httpx.Client(headers=HEADERS)
+
+
+def can_operate():
+    return not (CLIENT_ID is None or CLIENT_SECRET is None)
+
+
+def _save_token(**data):
+    token = OAuthToken(
+        service='spotify',
+        access_token=data.get('access_token'),
+        refresh_token=data.get('refresh_token'),
+        scope=data.get('scope'),
+        expires_at=timezone.now() + timedelta(seconds=data.get('expires_in', 0))
+    )
+    token.save()
+
+    return token
+
+
+def _refresh_token(token: OAuthToken):
+    try:
+        r = client.post(TOKEN_URL, data={
+            'grant_type': 'refresh_token',
+            'refresh_token': token.refresh_token
+        })
+        data = r.json()
+    except (httpx.RequestError, json.JSONDecodeError) as e:
+        print(e, file=sys.stderr)
+        return None
+
+    if r.status_code != 200:
+        print(f'Spotify error: {data}', file=sys.stderr)
+        return None
+
+    return _save_token(**data)
+
+
+def get_callback_url(redirect_uri, state_code):
+    qs = urlencode({
+        'response_type': 'code',
+        'client_id': CLIENT_ID,
+        'scope': SCOPES,
+        'redirect_uri': redirect_uri,
+        'show_dialog': True,
+        'state': state_code
+    })
+    return AUTHORIZE_CALLBACK + '?' + qs
+
+
+def resolve_access_token(code, redirect_uri):
+    try:
+        r = client.post(TOKEN_URL, data={
+            'grant_type': 'authorization_code',
+            'code': code,
+            'redirect_uri': redirect_uri
+        })
+        data = r.json()
+    except (httpx.RequestError, json.JSONDecodeError) as e:
+        print(e, file=sys.stderr)
+        return None
+
+    if r.status_code != 200:
+        print(f'Spotify error: {data}', file=sys.stderr)
+        return None
+
+    return _save_token(**data)
+
+
+def get_access_token():
+    try:
+        token = OAuthToken.objects.get(service='spotify')
+    except OAuthToken.DoesNotExist:
+        return None
+
+    if token.expires_at < timezone.now():  # refresh token
+        token = _refresh_token(token)
+    return token
+
+
+def get_user_profile(token: OAuthToken):
+    try:
+        r = client.get(f'{API_ENDPOINT}/me', headers={
+            'Authorization': f'Bearer {token.access_token}'
+        })
+        data = r.json()
+    except (httpx.RequestError, json.JSONDecodeError) as e:
+        print(e, file=sys.stderr)
+        return None
+
+    if r.status_code != 200:
+        print(f'Spotify error: {data}', file=sys.stderr)
+        return None
+
+    return data
--- apps/api/endpoints/spotify/views.py
+++ apps/api/endpoints/spotify/views.py
@@ -1,96 +1,28 @@
-import base64
-import random
-import string
-from datetime import datetime
-from urllib.parse import urlencode
-
-import httpx
 from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 
-CLIENT_ID = settings.CONFIG.get('spotify.clientId')
-CLIENT_SECRET = settings.CONFIG.get('spotify.clientSecret')
+from apps.api.models import SpotifyStateCode
+from . import spotify_api
+
 ALLOWED_EMAILS = settings.CONFIG.get('spotify.allowedEmails')
-
-HEADERS = {
-    'Authorization': 'Basic ' + base64.b64encode(f'{CLIENT_ID}:{CLIENT_SECRET}'.encode()).decode()
-}
-
-TOKEN_URL = 'https://accounts.spotify.com/api/token'
-AUTHORIZE_CALLBACK = 'https://accounts.spotify.com/authorize'
-SCOPES = 'user-read-private user-read-email'
-
-client = httpx.Client()
-
-
-# In-memory state tracker. Provides protection against XSS during the authorization flow.
-class StateTracker:
-    STATE_TIMEOUT = settings.CONFIG.get('spotify.authorizeTimeout', 60 * 60)
-    STATE_SIZE = 16
-
-    def __init__(self):
-        self._state_codes = {}
-
-    def _get_state_code(self):
-        # technically not cryptographically secure, but that's not too important here
-        return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(self.STATE_SIZE))
-
-    def _clean(self):
-        """Remove expired state codes"""
-        now = datetime.now()
-        to_remove = set()
-        for state, exp in self._state_codes.items():
-            if (now - exp).seconds > self.STATE_TIMEOUT:
-                to_remove.add(state)
-
-        for state in to_remove:
-            del self._state_codes[state]
-
-    def generate(self):
-        """Generates, saves and returns a new state code."""
-        self._clean()
-
-        state = self._get_state_code()
-        self._state_codes[state] = datetime.now()
-
-        return state
-
-    def verify(self, state_code: string):
-        """Verifies a given state code against the ones that are saved.
-        Deletes the state from memory if found."""
-        self._clean()
-
-        expiry = self._state_codes.get(state_code)
-        if expiry is None:
-            return False
-
-        del self._state_codes[state_code]
-        return True
-
-
-states = StateTracker()
 
 
 def login(request: HttpRequest):
-    if CLIENT_ID is None or CLIENT_SECRET is None:
+    if not spotify_api.can_operate():
         return HttpResponse('Error: clientId or clientSecret is missing, update config!', status=500)
 
-    qs = urlencode({
-        'response_type': 'code',
-        'client_id': CLIENT_ID,
-        'scope': SCOPES,
-        'redirect_uri': request.build_absolute_uri(reverse(callback)),
-        'show_dialog': True,
-        'state': states.generate()
-    })
-    return redirect(AUTHORIZE_CALLBACK + '?' + qs)
+    url = spotify_api.get_callback_url(
+        request.build_absolute_uri(reverse(callback)),
+        SpotifyStateCode.generate().state_code
+    )
+    return redirect(url)
 
 
 def callback(request: HttpRequest):
     state = request.GET.get('state')
-    if state is None or not states.verify(state):
+    if state is None or not SpotifyStateCode.verify(state):
         # 400 bad request
         return HttpResponse('Error: missing or invalid state parameter', status=400)
 
@@ -105,15 +37,19 @@ def callback(request: HttpRequest):
         return HttpResponse('Error: missing code parameter', status=400)
 
     # exchange code for token
-    try:
-        r = client.post(TOKEN_URL, data={
-            'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': request.build_absolute_uri().split('?')[0]
-        }, headers=HEADERS)
-        print(r.json())
-    except (httpx.RequestError, httpx.DecodingError):
+    token = spotify_api.resolve_access_token(code, request.build_absolute_uri().split('?')[0])
+    if token is None:
         # 500 internal server error
         return HttpResponse('Error while retrieving or decoding authentication token', status=500)
 
-    return HttpResponse(f'Done! Code: ' + code)
+    profile = spotify_api.get_user_profile(token)
+    if profile is None:
+        # 500 internal server error
+        return HttpResponse('Error: could not retrieve account details', status=500)
+    email = profile.get('email', '<unknown>')
+
+    if email not in ALLOWED_EMAILS:
+        # 403 forbidden
+        return HttpResponse(f'Error: account is not in the allowlist (logged in as {email})', status=403)
+
+    return HttpResponse(f'Done! Logged in as: {profile["email"]}')
