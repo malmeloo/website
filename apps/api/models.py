bbe814a2709a21b3adb003535f258e21cbedfb93
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2023-02-28 16:46:03 +0100

    Make database fields for OAuth tokens larger

--- apps/api/models.py
+++ apps/api/models.py
@@ -5,7 +5,7 @@ from .endpoints.spotify.models import SpotifyStateCode
 
 class OAuthToken(models.Model):
     service = models.CharField(primary_key=True, max_length=20)
-    access_token = models.CharField(max_length=100)
-    refresh_token = models.CharField(max_length=100)
+    access_token = models.CharField(max_length=255)
+    refresh_token = models.CharField(max_length=255)
     scope = models.CharField(max_length=100)
     expires_at = models.DateTimeField()
