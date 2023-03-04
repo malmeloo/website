78d3ddb08e2da5f8b30284025962d5328f5955b8
Mike A <32306794+DismissedGuy@users.noreply.github.com>
2023-03-04 18:59:06 +0100

    Cleanup

--- apps/api/endpoints/spotify/spotify_api.py
+++ apps/api/endpoints/spotify/spotify_api.py
@@ -1,6 +1,5 @@
 from base64 import b64encode
 
-import httpx
 from django.conf import settings
 
 from apps.api.models import OAuthToken
@@ -9,7 +8,6 @@ from apps.api.util.oauth2 import OAuth2Provider, request
 API_ENDPOINT = 'https://api.spotify.com/v1'
 SCOPES = ['user-read-email', 'user-top-read']
 
-client = httpx.Client()
 provider = OAuth2Provider(
     'spotify',
     'https://accounts.spotify.com/authorize',
@@ -28,6 +26,9 @@ def can_operate():
     return provider.can_operate
 
 
+##############################
+# Getting tokens / Auth flow #
+##############################
 def get_callback_url(redirect_uri, state_code):
     provider.set_redirect_uri(redirect_uri)
     return provider.create_auth_url(SCOPES, extra={'state': state_code, 'show_dialog': True})
@@ -40,9 +41,12 @@ def resolve_code(code, redirect_uri):
 
 
 def get_access_token():
-    return provider.get_access_token()
+    return provider.get_access_token(extra_headers=EXTRA_HEADERS)
 
 
+###############
+# API Methods #
+###############
 def get_user_profile(token: OAuthToken):
     return request('GET', API_ENDPOINT + '/me',
                    auth_token=token.access_token)
